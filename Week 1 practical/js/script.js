89d79fa00f1b06c5b629475d28dd60ecb786a451
Pohur Navin <navin.pohur@cygest.fr>
2026-07-01 11:41:38 +0400

    First setup commit

---
+++ Week 1 practical/js/script.js
@@ -0,0 +1,9 @@
+function showMore() {
+    let moreInfo = document.getElementById("moreInfo");
+
+    moreInfo.classList.toggle("hidden");
+}
+
+function changeTheme() {
+    document.body.classList.toggle("dark-mode");
+}
