a1ca4ca4fc30a66412b836463373eca53284a6e6
Pohur Navin <navin.pohur@cygest.fr>
2026-07-06 00:50:01 +0400

    new files added

---
+++ Week 2 practical/js/script.js
@@ -0,0 +1,21 @@
+function SubmitFunction(){
+    alert("hello navin");
+}
+
+
+/*
+
+explain: console.log, window.alert, localstorage, cookie
+
+Logic to write (15 mins)
+
+when user clicks on submit btn
+
+- if name, address and message is empty, alert user to fill in all the infos
+
+- if name is not empty but the rest are empty, alert user by their name and tell them to fill in the infos
+
+- if every field is not empty, when user clicks on submit, alert success
+
+
+*/
