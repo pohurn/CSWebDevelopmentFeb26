f873ab0d15d1a392f9758fcd0bba9150fb09fc70
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-07-07 14:36:03 +0400

    Updated the index.html file to include IDs for the input fields and textarea, and modified the script.js file to remove redundant alert checks and streamline the submission function.

--- Week 2 practical/js/script.js
+++ Week 2 practical/js/script.js
@@ -1,9 +1,37 @@
 function SubmitFunction(){
-    alert("hello navin");
+    //alert("Successfully submitted!");
+
+    var nameText = document.getElementById("name").value;
+    console.log(nameText);
+
+    var addressText = document.getElementById("address").value;
+    console.log(addressText);
+
+    var messageText = document.getElementById("message").value;
+    console.log(messageText);   
+
+    if(nameText == "" && addressText == "" && messageText == ""){
+        window.alert("Please Sadia, fill in your name, address and message");
+    }else{
+        window.alert("Successfully submitted!");
+    }
+
+    //if(nameText.value == ""){
+    //    window.alert("Please Sadia, fill in your name");
+    //}
+    //else if(addressText.value == ""){
+    //    window.alert("Please Sadia, fill in your address");
+    //}
+    //else if(messageText.value == ""){
+      //  window.alert("Please Sadia, fill in your message");
+    //}else{
+      //  window.alert("Successfully submitted!");
+    //}
+
+    return;
 }
 
-
-/*
+/*  
 
 explain: console.log, window.alert, localstorage, cookie
 
